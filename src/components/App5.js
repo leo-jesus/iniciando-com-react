c2bc5d48dae2d7ada44fee1f4491a5528bb31e29
leo-jesus <llxdgb1@gmail.com>
2021-07-08 22:38:56 -0300

    Life cicle of react's componets

---
+++ src/components/App5.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class App5 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { nome: undefined, txtNome: " " };
+  }
+
+  changetxtName = (event) => {
+    this.setState({ txtNome: event.target.value });
+  };
+
+  componentDidMount = () => {
+    const nome = sessionStorage.getItem("nome");
+    if (nome) {
+      this.setState({ nome });
+    }
+  };
+  persistName = () => {
+    this.setState({ nome: this.state.txtNome });
+    sessionStorage.setItem("nome", this.state.txtNome);
+  };
+  render() {
+    const renderForm = () => {
+      return (
+        <>
+          Nome:{" "}
+          <input
+            type="text"
+            placeholder="digite seu nome"
+            value={this.state.nome}
+            onChange={this.changetxtName}
+          ></input>
+          <button onClick={this.persistName}>Salvar</button>
+        </>
+      );
+    };
+    const renderText = () => {
+      return (
+        <>
+          <p>Olá {this.state.nome} </p>
+        </>
+      );
+    };
+
+    return !this.state.nome ? renderForm() : renderText();
+  }
+}
+
+export default App5;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import App from "./components/App";
 import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
+import App5 from "./components/App5";
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
 
     {/* <App2 title="Isso é um teste">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil officia,
