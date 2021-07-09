ca5bc1eb97774dc5cd07c5bbbe13dbefe3ddb442
leo-jesus <llxdgb1@gmail.com>
2021-07-08 23:40:42 -0300

    nested components

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -4,19 +4,22 @@ class Lista extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = [
-      { id: 1, nome: "item1", completo: false },
-      { id: 2, nome: "item2", completo: false },
-      { id: 3, nome: "item3", completo: true },
-      { id: 4, nome: "item4", completo: false },
-    ];
+    this.state = {
+      items: [
+        { id: 1, nome: "item1", completo: false },
+        { id: 2, nome: "item2", completo: false },
+        { id: 3, nome: "item3", completo: true },
+        { id: 4, nome: "item4", completo: false },
+      ],
+    };
   }
 
   render() {
     return (
       <ul>
-        {this.state.map((item) => (
-          <Item id={item.id} completo={item.completo}>
+        {React.Children.map((child) => (child.type == Item ? child : null))}
+        {this.state.items.map((item) => (
+          <Item key={item.id} id={item.id} completo={item.completo}>
             {item.nome}
           </Item>
         ))}
@@ -41,4 +44,4 @@ class Item extends React.Component {
   }
 }
 
-export default Lista;
+export { Lista, Item };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import App2 from "./components/App2";
-import App3 from "./components/App3";
-import App4 from "./components/App4";
-import App5 from "./components/App5";
-import Lista from "./components/Lista";
+// import App from "./components/App";
+// import App2 from "./components/App2";
+// import App3 from "./components/App3";
+// import App4 from "./components/App4";
+// import App5 from "./components/App5";
+import { Lista, Item } from "./components/Lista";
 
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Lista />
+    <Lista>
+      <Item key={999} id={999} completo={true}>
+        teste
+      </Item>
+      <Item key={1000} id={1000} completo={false}>
+        teste2
+      </Item>
+    </Lista>
 
     {/* <App2 title="Isso é um teste">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil officia,
