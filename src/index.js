f135b896779a617daee3e8d3618afe23a2fbbb2d
leo-jesus <llxdgb1@gmail.com>
2021-07-08 23:05:09 -0300

    List render

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5";
+import Lista from "./components/Lista";
+
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App5 />
+    <Lista />
 
     {/* <App2 title="Isso é um teste">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil officia,
